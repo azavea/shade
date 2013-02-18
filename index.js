b769397aa3c655842e4b8eca5d9f295d940c3a2f
Justin Walgran <jwalgran@azavea.com>
2013-02-18 17:13:28 -0500

    Add a load method
    I factored out some common path manipulation so it could be shared between
    the load and save methods.
    
    I implemented this in the simplest possible way using plain strings.
    This will cover my primary use case, but a future version of the module will
    be more generically useful if is supports reading and writing streams
    and handles character encoding properly.

--- index.js
+++ index.js
@@ -7,23 +7,30 @@ var Store = function(path) {
     this.path = path;
 };
 
+Store.prototype._createPaths = function(key) {
+    var keyDir = path.join(
+        this.path,
+        key.substring(0,2),
+        key.substring(2,4),
+        key.substring(4,6),
+        key.substring(6,8)
+    );
+    return {
+        keyDir: keyDir,
+        keyPath: path.join(keyDir, key)
+    };
+};
+
 Store.prototype.save = function(thing, cb) {
-    var sha1, key, keyDir, keyPath;
+    var sha1, key, paths;
     try {
         sha1 = crypto.createHash('sha1');
         sha1.update(thing);
         key = sha1.digest('hex').substring(0,8);
-        keyDir = path.join(
-            this.path,
-            key.substring(0,2),
-            key.substring(2,4),
-            key.substring(4,6),
-            key.substring(6,8)
-        );
-        keyPath = path.join(keyDir, key);
-        mkdirp(keyDir, function(err) {
+        paths = this._createPaths(key);
+        mkdirp(paths.keyDir, function(err) {
             if (!err) {
-                fs.writeFile(keyPath, thing, function(err) {
+                fs.writeFile(paths.keyPath, thing, function(err) {
                     if (cb) { cb(err, key); }
                 });
             } else {
@@ -33,6 +40,24 @@ Store.prototype.save = function(thing, cb) {
     } catch(err) {
         if (cb) { cb(err, undefined); }
     }
+};
+
+Store.prototype.load = function(key, cb) {
+    var paths = this._createPaths(key);
+    if (!cb) { return; }
+    fs.exists(paths.keyPath, function (exists) {
+        if (exists) {
+            fs.readFile(paths.keyPath, function (err, data) {
+                if (!err) {
+                    cb(null, data.toString());
+                } else {
+                    cb(err, undefined);
+                }
+            });
+        } else {
+            cb('unknown key', undefined);
+        }
+    });
 };
 
 exports = module.exports = function(p, cb) {
